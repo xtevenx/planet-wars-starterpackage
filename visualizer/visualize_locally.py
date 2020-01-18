90ae6af918163ec47dbc1286d8d536b468abd7bb
Steven Xia <stevenxia@protonmail.com>
2020-01-17 18:39:41 -0800

    Reformat 'visualize_locally' script.

--- visualizer/visualize_locally.py
+++ visualizer/visualize_locally.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python3
-
 import os
 import re
 import webbrowser
@@ -7,8 +5,8 @@ import webbrowser
 
 def generate(data, save_path):
     path = os.path.dirname(__file__)
-    template_path = os.path.join(path, 'index.php')
-    template = open(template_path, 'r')
+    template_path = os.path.join(path, "index.php")
+    template = open(template_path, "r")
     content = template.read()
     template.close()
 
@@ -16,7 +14,7 @@ def generate(data, save_path):
     javascript = "var data = '%s';" % data
     content = php_re.sub(javascript, content)
 
-    output = open(save_path, 'w+')
+    output = open(save_path, "w+")
     output.write(content)
     output.close()
 
@@ -25,7 +23,7 @@ if __name__ == "__main__":
     input_data = input()
 
     current_path = os.path.dirname(__file__)
-    generated_path = os.path.realpath(os.path.join(current_path, 'generated.html'))
+    generated_path = os.path.realpath(os.path.join(current_path, "generated.html"))
 
     generate(input_data, generated_path)
-    webbrowser.open('file://' + generated_path)
+    webbrowser.open("file://" + generated_path)
